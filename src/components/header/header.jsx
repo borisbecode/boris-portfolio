7c8979d18be38e0f762043d3546a03a11f568997
borisbecode <boris.lorenzonetto@gmail.com>
2022-02-18 21:57:39 +0100

    fonctionnel

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -12,10 +12,10 @@ export default function Header() {
   return (
     <div className="Header">
       <Grid container spacing={0}>
-        <Grid item xs={4} className="boris">
+        <Grid item xs={12} md={4} className="boris">
           <h1>Hello , I'm Boris !</h1>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={12} md={4}>
           <AvatarView
             style={{
               width: "400px",
@@ -29,7 +29,7 @@ export default function Header() {
             headMovement
           />
         </Grid>
-        <Grid item xs={4} className="end"></Grid>
+        <Grid item xs={0} md={4} className="end"></Grid>
       </Grid>
     </div>
   );
