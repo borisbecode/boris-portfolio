f14e13668e0e7a4a3a46e019313b34c97b2eeaf3
borisbecode <boris.lorenzonetto@gmail.com>
2022-02-18 21:05:46 +0100

    ok

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,9 @@
+import React from "react";
 import AvatarView from "@sarge/avatar-view";
 import "../../style/header.css";
+import Box from "@mui/material/Box";
+
+import Grid from "@mui/material/Grid";
 
 const url =
   "https://d1a370nemizbjq.cloudfront.net/58986872-a638-490e-b3f0-b3347718fec7.glb";
@@ -7,20 +11,25 @@ const url =
 export default function Header() {
   return (
     <div className="Header">
-      <div className="container">
-        <AvatarView
-          style={{
-            width: "400px",
-            height: "400px",
-            backgroundColor: "grey",
-            borderRadius: "100%",
-          }}
-          url={url}
-          rotateAvatar
-          eyeBlink
-          headMovement
-        />
-      </div>
+      <Grid container spacing={0}>
+        <Grid item xs className="boris">
+          <h1>Hello , I'm Boris !</h1>
+        </Grid>
+        <Grid item xs={6}>
+          <AvatarView
+            style={{
+              width: "100vh",
+              height: "450px",
+              backgroundColor: "rgb(34, 33, 33)",
+            }}
+            url={url}
+            rotateAvatar
+            eyeBlink
+            headMovement
+          />
+        </Grid>
+        <Grid item xs className="end"></Grid>
+      </Grid>
     </div>
   );
 }
